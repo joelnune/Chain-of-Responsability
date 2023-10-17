1f9e52a4796cbb2c4148d7860bbab7bc40312414
joel <joelnunes974@gmail.com>
2023-10-16 21:32:12 -0300

    commit

---
+++ main/java/com/mycompany/chain/Demo.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chain;
+
+package refactoring_guru.chain_of_responsibility.example;
+
+import com.mycompany.chain.Middleware;
+import com.mycompany.chain.RoleCheckMiddleware;
+import com.mycompany.chain.ThrottlingMiddleware;
+import com.mycompany.chain.UserExistsMiddleware;
+import com.mycompany.chain.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        // All checks are linked. Client can build various chains using the same
+        // components.
+        Middleware middleware = Middleware.link(
+            new ThrottlingMiddleware(2),
+            new UserExistsMiddleware(server),
+            new RoleCheckMiddleware()
+        );
+
+        // Server gets a chain from client code.
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+}
